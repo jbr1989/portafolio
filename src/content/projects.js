4ce377ffd266a405bdcde1da566ff7d032635ac5
Jaime Balague <jbr1989@gmail.com>
2025-01-20 12:11:22 +0100

    init

---
+++ src/content/projects.js
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+        img: "img/projects/twitch_chat.webp",
+        title: "CroquetaBot",
+        link: "https://github.com/jbr1989/CroquetaBot",
+        description: "Chatbot de Twitch para interactuar con los espectadores de un canal",
+        technologies: [
+            // {
+            //     icon: IconKeys.typeScript,
+            // },
+            // {
+            //     icon: IconKeys.nextjs,
+            // },
+            // {
+            //     icon: IconKeys.tailwind,
+            // },
+            // {
+            //     icon: IconKeys.zustand,
+            // },
+            // {
+            //     icon: IconKeys.shadcn,
+            // },
+        ],
+    },
+
+];
