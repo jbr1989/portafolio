038b135c734383e816c734ea5ee27b036188f69e
Jaime Balague <jbr1989@gmail.com>
2025-01-20 15:57:57 +0100

    Add project: Pokemon TCG Card Browser

--- src/content/projects.js
+++ src/content/projects.js
@@ -2,25 +2,18 @@ export const projects = [
     {
         img: "img/projects/twitch_chat.webp",
         title: "CroquetaBot",
-        link: "https://github.com/jbr1989/CroquetaBot",
+        code: "https://github.com/jbr1989/CroquetaBot",
+        link: "",
         description: "Chatbot de Twitch para interactuar con los espectadores de un canal",
-        technologies: [
-            // {
-            //     icon: IconKeys.typeScript,
-            // },
-            // {
-            //     icon: IconKeys.nextjs,
-            // },
-            // {
-            //     icon: IconKeys.tailwind,
-            // },
-            // {
-            //     icon: IconKeys.zustand,
-            // },
-            // {
-            //     icon: IconKeys.shadcn,
-            // },
-        ],
+        technologies: ["nodejs", "mysql", "tmijs"],
+    },
+    {
+        img: "img/projects/pokemon_cards.webp",
+        title: "Pokemon TCG Card Browser",
+        code: "https://github.com/jbr1989/pokemon_cards",
+        link: "https://pokemon.jbr1989.es/",
+        description: "Buscador de cartas de Pokemon TCG (EN DESARROLLO)",
+        technologies: [ "astro", "tailwind", "api" ]
     },
 
 ];
